dbef192069d05c0d8e103add3e9390a1dc8f1c9b
Ebee02 <beelawrence8@gmail.com>
2025-03-11 13:17:48 +0000

    Added path validation to the read_book func

--- book2.py
+++ book2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from pydantic import BaseModel, Field
 
 
@@ -60,7 +60,7 @@ async def read_all_books():
 
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
